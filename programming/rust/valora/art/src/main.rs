7ffa3aba440c8477c9e2719bae74f39194de83a7
Jéssica Lins <jessica.lins@ecosia.org>
2020-04-23 14:40:27 +0200

    Custom shaders

--- programming/rust/valora/art/src/main.rs
+++ programming/rust/valora/art/src/main.rs
@@ -1,16 +1,39 @@
 use valora::prelude::*;
 
+#[derive(UniformSet, Copy, Clone, Debug)]
+struct Uniforms {
+    /// A solid color overlay on the shader pattern.
+    color: (f32, f32, f32),
+    /// The vector scale of the output.
+    scale: f32,
+    /// Width of the output.
+    width: f32,
+    /// Height of the output.
+    height: f32,
+}
+
 fn main() -> Result<()> {
-    run_fn(Options::from_args(), |_gpu, world, _rng| {
+    run_fn(Options::from_args(), |gpu, world, _rng| {
+        let mut program = ShaderProgram::new(&gpu, "pattern.frag")?;
+        let mut uniforms = Uniforms {
+            color: Hsv::new(0., 0.7, 0.7).into_rgb::<Srgb>().into_components(),
+            scale: world.scale,
+            width: world.width,
+            height: world.height,
+        };
+
         Ok(move |ctx: Context, canvas: &mut Canvas| {
             canvas.set_color(LinSrgb::new(1., 1., 1.));
             canvas.paint(Filled(ctx.world));
 
-            let max_radius = world.width / 3.;
-            let radius = ctx.time.as_secs_f32().cos().abs() * max_radius;
+            let hue = ctx.time.as_secs_f32().sin().abs() * 40.;
+            uniforms.color = Hsv::new(hue, 0.7, 0.7).into_rgb::<Srgb>().into_components();
+            // canvas.set_shader(shader.clone());
+            canvas.set_shader(program.bind(uniforms));
 
-            canvas.set_color(LinSrgb::new(1., 0., 0.));
-            canvas.paint(Filled(Ellipse::circle(world.center(), radius)));
+            let square = Ngon::square(world.center(), 200.);
+            
+            canvas.paint(Filled(square));
         })
     })
 }
